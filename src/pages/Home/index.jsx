ffdbdf9bdd57f26a5cf7da3dbc8d5dbbc1b22480
Rahmat ullah badsha <87254817+Tamhat@users.noreply.github.com>
2023-08-31 21:19:58 +0600

    New update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,37 @@
 import React, { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import brand from "../Room/video-logo.jfif";
 
 const HomePage = () => {
   const [value, setValue] = useState();
   const navigate = useNavigate();
 
   const handleJoinRoom = useCallback(() => {
-
-     navigate(`/room/${value}`);
-
+    navigate(`/room/${value}`);
   }, [navigate, value]);
 
   return (
-    <div>
-      <input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        type="text"
-        placeholder="Enter Room Code"
-      />
-      <button className="join" onClick={handleJoinRoom}>
-        Join
-      </button>
+    <div className="d-block">
+      <img src={brand} alt="brand" className="img-fluid brand-image" />
+      <div className="input-group mb-3 w-75 position-relative top-50 start-50 translate-middle">
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          type="text"
+          className="form-control"
+          placeholder="Enter Room Code"
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+        />
+        <button
+          className="btn btn-outline-secondary join join-btn"
+          type="submit"
+          id="button-addon2"
+          onClick={handleJoinRoom}
+        >
+          Join
+        </button>
+      </div>
     </div>
   );
 };
